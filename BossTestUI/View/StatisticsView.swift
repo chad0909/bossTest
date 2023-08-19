762a01a80bcacc6c13da388a26d36c715783d177
radin <dlwlsgml8847@gmail.com>
2023-08-19 22:04:49 +0900

    상단 탭 수정

--- BossTestUI/View/StatisticsView.swift
+++ BossTestUI/View/StatisticsView.swift
@@ -7,9 +7,49 @@
 
 import SwiftUI
 
+enum TitleSection: String {
+    case published, used
+    
+    var rawValue: String {
+        switch self {
+        case .published:
+            return "발급완료"
+        case .used:
+            return "사용완료"
+        }
+    }
+}
+
+struct TitleTextView: View {
+    let section: TitleSection
+    let namespace: Namespace.ID
+    @Binding var selectedSection: TitleSection
+    
+    var body: some View {
+        Text(section.rawValue)
+            .font(.system(size: 20, weight: .semibold))
+            .foregroundColor(section == selectedSection ? .black : .gray)
+            .padding(.horizontal, 20)
+            .background(
+                ZStack {
+                    if section == selectedSection {
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(height: 2)
+                            .offset(y: 18)
+                            .matchedGeometryEffect(id: "DetailSectionCard", in: namespace)
+                    }
+                }
+            )
+            .padding()
+    }
+}
+
 struct StatisticsView: View {
     @State private var selectedTab = 0
-    
+    @Namespace var namespace
+    @State private var selectedSection = TitleSection.published
+    let sectionList: [TitleSection] = [.published, .used]
+
     var body: some View {
         NavigationView{
             ZStack{
@@ -17,14 +57,25 @@ struct StatisticsView: View {
                     
                     ScrollView{
                         VStack(spacing: 0){
-                            Picker("", selection: $selectedTab) {
-                                Text("발급 완료").tag(0)
-                                Text("사용 완료").tag(1)
+                            HStack(spacing: 30) {
+                                ForEach(sectionList, id: \.self) { section in
+                                    TitleTextView(section: section, namespace: namespace, selectedSection: $selectedSection)
+                                        .onTapGesture {
+                                            withAnimation(.easeOut) {
+                                                selectedSection = section
+                                            }
+                                        }
+                                }
                             }
-                            .pickerStyle(.segmented)
-                            .padding()
+                            .frame(maxWidth: .infinity)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(height: 1)
+                                    .foregroundColor(.gray)
+                                    .offset(y: 18)
+                            }
                             
-                            if selectedTab == 0 {
+                            if selectedSection == .published {
                                 StatisticsSubmitFinishedTabView()
                             } else {
                                 StatisticsUsedFinishedTabView()
